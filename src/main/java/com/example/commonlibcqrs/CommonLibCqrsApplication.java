b37df67eaacfe095e5d889747c3ca3aa9ebab4c2
thainguyenvu <nvthai1602@gmail.com>
2025-10-31 14:57:22 +0700

    Update pom.xml

---
+++ src/main/java/com/example/commonlibcqrs/CommonLibCqrsApplication.java
@@ -0,0 +1,11 @@
+package com.example.commonlibcqrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonLibCqrsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CommonLibCqrsApplication.class, args);
+    }
+}
